8a5e733cdd046c8e704cd60e2b219209038f6484
Thomas Michiels <michiels.thomas@gmail.com>
2011-05-27 14:05:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luxomattic::Application.config.secret_token = 'a77407ab0f5163e8dfd0b56a1682ff97203bd3667984b416e1a97c11318369e29ff41589579347d54f87cec94dfcd7f10419f4935a8636faad88dbdb3c6e24e9'
